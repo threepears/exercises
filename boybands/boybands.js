cabff465b89d8abe7415532feba3c5071f511d2f
threepears <michael@threepears.com>
2015-10-13 10:56:34 -0500

    Changes to index and js, new style file

--- boybands/boybands.js
+++ boybands/boybands.js
@@ -2,7 +2,7 @@ var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // The number of loops to perform (what if the array changes?)
-var loopCount = 5;
+var loopCount = bands.length;
 
 // Keep track of which band we're on in the loop
 var currentBand = "";
@@ -11,19 +11,23 @@ var currentBand = "";
 var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
+var bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
+var veggieElement = document.getElementById("vegetables");
 
 // Start looping
 for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
 
   // Add the band names into the correct <div>
-  currentBand = ???;
+  currentBand = bands[loopTracker];
+  var bandCode = "<div>" + currentBand + "</div>"
+  bandElement.innerHTML = bandElement.innerHTML + bandCode;
 
 
   // Add the veggie names into the correct <div>
-  currentVeggie = ???;
+  currentVeggie = vegetables[loopTracker];
+  var vegetableCode = "<div>" + currentVeggie + "</div>"
+  veggieElement.innerHTML = veggieElement.innerHTML + vegetableCode;
 
 }
