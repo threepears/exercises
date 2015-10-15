c9d8285becdf09ac42395b2e14509386fc33cc80
threepears <michael@threepears.com>
2015-10-15 14:44:48 -0500

    Add files

---
+++ arraymethods/basic-array-methods/arrays.js
@@ -0,0 +1,29 @@
+/*
+  ...........YOUR MISSION...........
+
+  Write JavaScript functions that perform the following tasks.
+
+  1. document.write() the characters of any string in reverse order.
+  2. document.write() the characters of any string in alphabetical order.
+  3. document.write() true, or false, if the passed string is a palidrome.
+
+  Note: document.write() is a fast way to put content into the DOM 
+  without the need for a getElementById() and innerHTML.
+*/
+
+function reversal(origString) {
+
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
